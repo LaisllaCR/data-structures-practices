606926fb553757ab7ea1329f3faa80c8f65c2723
lramos <laisllaramos@gmail.com>
2020-06-24 23:24:35 -0400

    2 Tree challenges: Height of a Binary Tree and Level Order Traversal

---
+++ HeightBinaryTree/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace HeightBinaryTree
+{
+    public class Node
+    {
+        public int data;
+        public Node left;
+        public Node right;
+
+        public Node(int value)
+        {
+            data = value;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int totalNodes = 7;
+            int[] nodesValues = new int[totalNodes] { 3, 5, 2, 1, 4, 6, 7 };
+            
+            Node root = new Node(nodesValues[0]);
+
+            for(int i = 1; i < totalNodes; i++)
+            {
+                Insert(root, nodesValues[i]);
+            }
+
+            int height = GetTreeHeight(root);
+
+            Console.WriteLine("Height of the Binary Tree: "+ height);
+        }
+
+        public static int GetTreeHeight(Node root)
+        {
+            if(root == null)
+            {
+                return -1;
+            }
+
+            int left = GetTreeHeight(root.left);
+            int right = GetTreeHeight(root.right);
+
+            return Math.Max(left, right) + 1;
+        }
+
+        public static Node Insert(Node root, int data)
+        {
+            if (root == null)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node cur;
+                if (data <= root.data)
+                {
+                    cur = Insert(root.left, data);
+                    root.left = cur;
+                }
+                else
+                {
+                    cur = Insert(root.right, data);
+                    root.right = cur;
+                }
+                return root;
+            }
+        }
+    }
+}
---
+++ LevelOrderTraversal/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LevelOrderTraversal
+{
+    public class Node
+    {
+        public int data;
+        public Node left;
+        public Node right;
+
+        public Node(int value)
+        {
+            data = value;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int totalNodes = 6;
+            int[] nodeValues = new int[totalNodes] { 1, 2, 5, 3, 6, 4 };
+            Node root = new Node(nodeValues[0]);
+
+            for(int i = 1; i < totalNodes; i++)
+            {
+                Insert(root, nodeValues[i]);
+            }
+
+            TraversalOrderWithBfs(root);
+        }
+
+        public static void TraversalOrderWithBfs(Node root)
+        {
+            if(root == null)
+            {
+                Console.WriteLine("No Tree.");
+            }
+            else
+            {
+                Queue<Node> queue = new Queue<Node>();
+
+                queue.Enqueue(root);
+
+                while(queue.Count > 0)
+                {
+                    for(int i = 0; i < queue.Count; i++)
+                    {
+                        Node current = queue.Dequeue();
+
+                        Console.Write(current.data + " ");
+
+                        if(current.left != null)
+                        {
+                            queue.Enqueue(current.left);
+                        }
+
+                        if (current.right != null)
+                        {
+                            queue.Enqueue(current.right);
+                        }
+                    }
+                }
+            }
+        }
+
+        public static Node Insert(Node root, int data)
+        {
+            if(root == null)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node current = root;
+                if(data <= current.data)
+                {
+                    current = Insert(root.left, data);
+                    root.left = current;
+                }
+                else
+                {
+                    current = Insert(root.right, data);
+                    root.right = current;
+                }
+                return root;
+            }
+        }
+    }
+}
